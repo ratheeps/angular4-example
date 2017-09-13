edee5cf34f2606c55a59a6386ac54464f5a5a299
Ratheep <ratheep.ceymplon@jeylabs.com.au>
2017-09-13 09:43:34 +0530

    router init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
 import { LoginComponent } from './component/login/login.component';
-
+import {RouterModule} from '@angular/router';
+import {routes} from './app.routes';
 
 
 @NgModule({
@@ -11,7 +12,9 @@ import { LoginComponent } from './component/login/login.component';
   LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [DashboardComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+﻿import {Routes} from '@angular/router';
+
+import {
+  DashboardComponent
+} from './component/master';
+
+export const routes: Routes = [
+    {path: '', component: DashboardComponent, pathMatch: 'full'},
+];
